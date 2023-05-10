f5f22b0f59af424661433d2901e660bb577ed212
Rick <ehmplankje@gmail.com>
2023-05-10 23:39:39 +0200

    GEN documentation

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # HAMMOCK GPT
+This project provides an add-on for Microsoft Outlook that allows users to select a piece of text within an email and perform one of three operations: Proofread the text, translate the text to a specified language, or change the sentiment of the text. The operations are powered by the OpenAI API.
 
-## https://learn.microsoft.com/en-us/office/dev/add-ins/quickstarts/outlook-quickstart?tabs=yeomangenerator
+## Getting Started
+To get a local copy up and running, follow these steps:
+
+## Prerequisites
+* Node.js and NPM installed on your local development machine.
+* An API Key from OpenAI.
+
+## Installation
+1) Clone the repo:
+    ```bash
+    git clone git@github.com:ehmPlankje/hammock-gpt.git
+    ```
+   
+2) Install NPM packages:
+    ```bash
+    npm install
+    ```
+3) Create a `.env` file in the `/webpack` directory by copying the `/webpack/.env.sample` file. Set the `OPENAI_API_KEY` value with your own OpenAI key.
+
+## Running the Project
+Start the development server:
+    ```bash
+    npm run dev-server
+    ```
+
+## Sideload the Add-On in Outlook
+In order to load the add-on in Outlook, follow the instructions provided by Microsoft on [how to sideload Outlook add-ins for testing](https://learn.microsoft.com/en-us/office/dev/add-ins/outlook/sideload-outlook-add-ins-for-testing?tabs=web). You'll need to use the `manifest.xml` file located in the `/assets/manifests` directory.
+
+## SSL Certificate
+This project uses a self-signed SSL certificate for local development, which is defined in the `/webpack/webpack.dev.js` file. For security reasons, you should replace this with your own SSL certificate. A valid SSL certificate is required by Microsoft Outlook.
+
+You can generate a self-signed certificate following the instructions on [this page](https://devcenter.heroku.com/articles/ssl-certificate-self). After generating your certificate, you must add it to your trusted root authorities. The process varies depending on your OS and browser.
+
+## Usage
+Once the add-on is sideloaded in Outlook, you can select any text in an email. After selecting the text, you can choose one of the available operations: Proofread, Translate, or Change Sentiment. The add-on will then call the OpenAI API to perform the requested operation and display the transformed text, which you can then accept to replace the original selected text.
+
+## Contact
+Please contact me via my contact details listed within Canvas if you need any assistance or require more information.
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+// Import necessary dependencies
 import * as React from 'react'
 import { ReactElement, useEffect, useState } from 'react'
 import { Stack, StackItem } from '@fluentui/react'
@@ -10,7 +11,8 @@ import {
     MenuItem,
     MenuList,
     MenuPopover,
-    MenuTrigger, Spinner,
+    MenuTrigger,
+    Spinner,
     Text,
     Textarea,
 } from '@fluentui/react-components'
@@ -20,14 +22,16 @@ import {
     Pen24Regular,
     Translate24Regular,
 } from '@fluentui/react-icons'
-import axios from 'axios'
+import axios from 'axios' // To make HTTP requests
 
+// Define the operations that can be performed on the text
 enum Operation {
     Proofread = 'proofread',
     Translate = 'translate',
     Sentiment = 'change sentiment'
 }
 
+// Define styles for the component
 const useStyles = makeStyles({
     button: {
         width: '100%',
@@ -39,9 +43,11 @@ const useStyles = makeStyles({
     },
 })
 
+// Define the languages and sentiments that can be used for translation and sentiment change
 const languages = ['English', 'Dutch', 'French', 'Spanish', 'German', 'Russian']
 const sentiments = ['Layman', 'Friendly', 'Formal', 'Informal', 'Assertive', 'Empathetic', 'Sarcastic', 'Emoji']
 
+// Function to construct the query based on the operation and parameter
 const getQuery = (op: Operation, param?: string): string => {
     switch (op) {
         case Operation.Translate:
@@ -52,6 +58,8 @@ const getQuery = (op: Operation, param?: string): string => {
             return `Please correct and improve the following text in its source language, but don't change the source language:`
     }
 }
+
+// Function to perform the transformation operation using OpenAI's API
 const transform = (op: Operation, text: string, setTransformation: (transformation: string) => void, param?: string): void => {
     const headers = {
         'Content-Type': 'application/json',
@@ -72,26 +80,30 @@ const transform = (op: Operation, text: string, setTransformation: (transformati
         ],
     }
 
+    // Make a POST request to the OpenAI API
     axios.post('https://api.openai.com/v1/chat/completions', data, {headers: headers})
-        .then(response => setTransformation(response.data.choices[0].message.content))
+        .then(response => setTransformation(response.data.choices[0].message.content)) // Set the transformation state with the response from the API
 }
 
-
+// Define the main App component
 export default function App(): ReactElement {
-    const styles = useStyles()
-    const [selection, setSelection] = useState<string>('')
-    const [transformation, setTransformation] = useState<string>('')
-    const [loading, setLoading] = useState<boolean>(false)
+    const styles = useStyles() // Use the styles defined above
+    const [selection, setSelection] = useState<string>('') // State for the selected text
+    const [transformation, setTransformation] = useState<string>('') // State for the transformed text
+    const [loading, setLoading] = useState<boolean>(false) // State for the loading status
 
+    // Function to handle button click events
     const clickButton = (op: Operation, param?: string) => {
-        setLoading(true)
-        transform(op, selection, setTransformation, param)
+        setLoading(true) // Set loading status to true
+        transform(op, selection, setTransformation, param) // Call the transform function with the selected operation, text, transformation state setter, and parameter
     }
 
+    // When the transformation state changes, set loading status to false
     useEffect(() => {
         setLoading(false)
     }, [transformation])
 
+    // When the component mounts, start an interval to get the selected text in the Outlook email every 100ms
     useEffect(() => {
         const intervalId = setInterval(() => {
             Office.context.mailbox.item.getSelectedDataAsync(Office.CoercionType.Text, result => {
@@ -99,15 +111,18 @@ export default function App(): ReactElement {
             })
         }, 100)
 
+        // When the component unmounts, clear the interval
         return () => {
             clearInterval(intervalId)
         }
     }, [])
 
+    // Render the component
     return (
         <Stack tokens={{childrenGap: 'm'}}>
             <Text size={600}>Select any text fragment</Text>
             <Stack tokens={{childrenGap: 's1'}}>
+                {/* Button to start proofreading operation */}
                 <StackItem grow>
                     <Button
                         className={styles.button}
@@ -118,6 +133,7 @@ export default function App(): ReactElement {
                         Proofread
                     </Button>
                 </StackItem>
+                {/* Dropdown menu to select language for translation */}
                 <StackItem grow>
                     <Menu>
                         <MenuTrigger disableButtonEnhancement>
@@ -144,6 +160,7 @@ export default function App(): ReactElement {
                         </MenuPopover>
                     </Menu>
                 </StackItem>
+                {/* Dropdown menu to select sentiment for sentiment change */}
                 <StackItem grow>
                     <Menu>
                         <MenuTrigger disableButtonEnhancement>
@@ -171,11 +188,14 @@ export default function App(): ReactElement {
                     </Menu>
                 </StackItem>
             </Stack>
+            {/* Show spinner when loading */}
             {loading &&
                 <StackItem>
-                    <Spinner labelPosition="below" label="Querying ChatGPT..."/>
+                    <Spinner labelPosition="below"
+                             label="Querying ChatGPT..."/> {/* Spinner to indicate loading when querying the ChatGPT API */}
                 </StackItem>
             }
+            {/* Button to accept the transformation*/}
             {transformation != '' &&
                 <StackItem>
                     <Button
@@ -186,9 +206,9 @@ export default function App(): ReactElement {
                             Office.context.mailbox.item.body.setSelectedDataAsync(
                                 transformation,
                                 {coercionType: Office.CoercionType.Text},
-                            )
-                            setSelection('')
-                            setTransformation('')
+                            ) /* Replace the selected text in the Outlook email with the transformed text */
+                            setSelection('') /* Reset selection state */
+                            setTransformation('') /* Reset transformation state */
                         }}
                         icon={<CommentMultipleCheckmark24Regular/>}
                     >
@@ -196,24 +216,28 @@ export default function App(): ReactElement {
                     </Button>
                 </StackItem>
             }
+            {/* Show the selected text when there's no transformation */}
             {transformation == '' &&
                 <StackItem>
                     <Field label="Selection">
                         <Textarea
-                            value={selection}
-                            readOnly size={'small'}
+                            value={selection} /* Display the selected text */
+                            readOnly /* Make the text area read-only */
+                            size={'small'}
                             resize={'vertical'}
                             className={styles.textArea}
                         />
                     </Field>
                 </StackItem>
             }
+            {/* Show the transformed text when there is a transformation */}
             {transformation != '' &&
                 <StackItem>
                     <Field label="Transformation">
                         <Textarea
-                            value={transformation}
-                            readOnly size={'small'}
+                            value={transformation} /* Display the transformed text */
+                            readOnly /* Make the text area read-only */
+                            size={'small'}
                             resize={'vertical'}
                             className={styles.textArea}
                         />
